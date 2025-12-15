445e918deaf33402420ce6a79bb777c2fbdf081d
ameer suhail <suhail@ameers-MacBook-Air.local>
2025-12-15 23:49:11 +0530

    FCD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
-import About from "./pages/About"
+
 import Experience from "./pages/Experience"
 import Contact from "./pages/Contact"
 import AboutSection from "./pages/About"
